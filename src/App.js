39d53af601949637ad182e5b9fb296b1c5dac681
Яков Носков <jakovnoskov@hotmail.com>
2022-09-30 18:51:23 +0600

    fixddt

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 
 
-import Card from './components/Card';
-import Header from './components/Header';
-import Drawer from './components/Drawer';
+import Card from './components/Card'
+import Header from './components/Header'
+import Drawer from './components/Drawer'
 
 const arr = [
-  {title:'Мужские Кроссовки Nike Blazer Mid Suede', price: 12999, imageUrl:'/img/sneakers/1.jpeg'},
-  {title:'Мужские Кроссовки Nike Air Max 270', price: 12999, imageUrl:'/img/sneakers/2.jpeg'},
-  {title:'Мужские Кроссовки Nike Blazer Mid Suede', price: 12999, imageUrl:'/img/sneakers/3.jpeg'},
-  {title:'Кроссовки Puma X Aka Boku Future Rider', price: 12999, imageUrl:'/img/sneakers/4.jpeg'},
-  {title:'Мужские Кроссовки Under Armour Curry 8', price: 12999, imageUrl:'/img/sneakers/5.jpeg'},
-  {title:'Мужские Кроссовки Nike Kyrie 7', price: 12999, imageUrl:'/img/sneakers/6.jpeg'},
-  {title:'Мужские Кроссовки Jordan Air Jordan 11', price: 12999, imageUrl:'/img/sneakers/7.jpeg'},
-  {title:'Мужские Кроссовки Nike LeBron XVIII', price: 12999, imageUrl:'/img/sneakers/8.jpeg'},
-  {title:'Мужские Кроссовки Nike Lebron XVIII Low', price: 12999, imageUrl:'/img/sneakers/9.jpeg'},
-  {title:'Мужские Кроссовки Nike Blazer Mid Suede', price: 12999, imageUrl:'/img/sneakers/10.jpeg'},
-  {title:'Кроссовки Puma X Aka Boku Future Rider', price: 12999, imageUrl:'/img/sneakers/11.jpeg'},
-  {title:'Мужские Кроссовки Nike Kyrie Flytrap IV', price: 12999, imageUrl:'/img/sneakers/12.jpeg'}
+  {id:1, title:'Мужские Кроссовки Nike Blazer Mid Suede', price: 12999, imageUrl:'/img/sneakers/1.jpeg'},
+  {id:2, title:'Мужские Кроссовки Nike Air Max 270', price: 12999, imageUrl:'/img/sneakers/2.jpeg'},
+  {id:3, title:'Мужские Кроссовки Nike Blazer Mid Suede', price: 12999, imageUrl:'/img/sneakers/3.jpeg'},
+  {id:4, title:'Кроссовки Puma X Aka Boku Future Rider', price: 12999, imageUrl:'/img/sneakers/4.jpeg'},
+  {id:5, title:'Мужские Кроссовки Under Armour Curry 8', price: 12999, imageUrl:'/img/sneakers/5.jpeg'},
+  {id:6, title:'Мужские Кроссовки Nike Kyrie 7', price: 12999, imageUrl:'/img/sneakers/6.jpeg'},
+  {id:7, title:'Мужские Кроссовки Jordan Air Jordan 11', price: 12999, imageUrl:'/img/sneakers/7.jpeg'},
+  {id:8, title:'Мужские Кроссовки Nike LeBron XVIII', price: 12999, imageUrl:'/img/sneakers/8.jpeg'},
+  {id:9, title:'Мужские Кроссовки Nike Lebron XVIII Low', price: 12999, imageUrl:'/img/sneakers/9.jpeg'},
+  {id:10, title:'Мужские Кроссовки Nike Blazer Mid Suede', price: 12999, imageUrl:'/img/sneakers/10.jpeg'},
+  {id:11, title:'Кроссовки Puma X Aka Boku Future Rider', price: 12999, imageUrl:'/img/sneakers/11.jpeg'},
+  {id:12, title:'Мужские Кроссовки Nike Kyrie Flytrap IV', price: 12999, imageUrl:'/img/sneakers/12.jpeg'}
 ]
 
 function App() {
@@ -39,6 +39,7 @@ function App() {
         <div className="productWrapper">
         {arr.map((obj) => (
            <Card 
+            key={obj.id}
             title={obj.title} 
             price={obj.price} 
             imageUrl={obj.imageUrl} 
@@ -47,7 +48,7 @@ function App() {
         </div>                   
       </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,17 +1,15 @@
-import styles from './Card.module.scss';
+import styles from './Card.module.scss'
 
 
 
 function Card(props) {
     return (
         <div className={styles.card}>
-
             <div className={styles.favorite}>  
-            <img width="32" height="32" src="/img/heart-unliked.svg" alt="Unliked"/>
+                <img width="32" height="32" src="/img/heart-unliked.svg" alt="Unliked"/>
             </div>
-
-        <img width={133} height={112} src={props.imageUrl}/>
-        <h5>{props.title}</h5>
+            <img width={133} height={112} src={props.imageUrl}/>
+            <h5>{props.title}</h5>
             <div className={styles.cardBottom}>
             <div className={styles.cardInfo}>
                 <span>Цена</span>
@@ -21,9 +19,8 @@ function Card(props) {
                 <img width="11" height="11" src="/img/plus.svg"/>
             </button>
             </div>
-
         </div>
-    );
+    )
 }
 
-export default Card;
+export default Card
