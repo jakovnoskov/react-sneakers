1735854e12ba769015e9265dd0a51a0b1d2ff09d
Яков Носков <jakovnoskov@hotmail.com>
2022-10-10 17:07:24 +0600

    fix change color

--- src/routes/Detail/index.jsx
+++ src/routes/Detail/index.jsx
@@ -1,145 +1,108 @@
 import React from 'react'
-import { 
-  useParams, 
-  //useNavigate, 
-  Link } from 'react-router-dom'
-import ContentLoader from 'react-content-loader'
+import { useParams,Link } from 'react-router-dom'
 import styles from './Detail.module.scss'
 import AppContext from '../../context'
 import GlobalLoader from '../../components/GlobalLoader'
 import {getCurrentDate} from '../../utils/getCurrentDate'
 
-function Detail() {
-    const {
-        isItemAdded,
-        isItemFavorited,        
-        onAddToFavorite,
-        onAddToCard,
-        setCartOpened,
-        isLoading,
-        sneakers,
-        isLoadingFavorite,
-        showCase
-    } = React.useContext(AppContext)
+export default function Detail() {
+const {
+isItemAdded,
+isItemFavorited,        
+onAddToFavorite,
+onAddToCard,
+setCartOpened,
+isLoading,
+sneakers,
+isLoadingFavorite,
+showCase
+} = React.useContext(AppContext)
 
-    const[isFavorite, setIsFavorite] = React.useState(false)
-    const [item, setItem] = React.useState({})
-    const [checkedColor1, setCheckedColor1] = React.useState(false);
-    const [checkedColor2, setCheckedColor2] = React.useState(false);
-    const [checkedColor3, setCheckedColor3] = React.useState(false);
+const[isFavorite, setIsFavorite] = React.useState(false)
+const [item, setItem] = React.useState({})
+const [color, setColor] = React.useState({})
 
-    let params = useParams()
-    //const navigate = useNavigate();
-    const idInUrl = Number(params.productId.replace(/[^\d\+]/g, ''))
+let params = useParams()
+const idInUrl = Number(params.productId.replace(/[^\d\+]/g, ''))
 
-    React.useEffect(() => {
-       if (sneakers && sneakers.length > 0) setItem(sneakers.find(objF => Number(objF.productId) === idInUrl))
-    }, [sneakers])
+React.useEffect(() => {
+if (sneakers && sneakers.length > 0) setItem(sneakers.find(objF => Number(objF.productId) === idInUrl))
+}, [sneakers])
 
-    const onClickFovarite = () => {
-        onAddToFavorite(item)
-        setIsFavorite(!isFavorite)
+const onClickFovarite = () => {
+onAddToFavorite(item)
+setIsFavorite(!isFavorite)
+}
+
+const onClickColor = (num) => setColor(num)
+
+return (
+<div className="content">
+{isLoading ? <GlobalLoader /> : (
+<>
+  <div className={styles.detailProductContent}>
+    <Link to={`${showCase}`}>
+      <img 
+      className={styles.backBtn} 
+      width="35" 
+      height="35" 
+      src="img/btn-back.svg" 
+      alt="Back"
+      />
+    </Link>
+    <h1 className={styles.titleContent}>{item.title}</h1>
+  </div> 
+<div className={styles.detailPageWrapper}>
+  <div className={styles.imgProductBox}>
+    <img  width={133} src={item.imageUrl}/>
+  </div>
+<div className={styles.infoProductBox}>
+<div className={styles.productPrice}>{item.price} ₽</div>
+<div className={styles.colorTitle}>Цвет:</div>
+<ul className={styles.colorBox}>
+<li onClick={() => onClickColor(1)} className={`${color === 1 ? styles.activeColor : ''}`}/>
+<li onClick={() => onClickColor(2)} className={`${color === 2 ? styles.activeColor : ''}`}/>
+<li onClick={() => onClickColor(3)} className={`${color === 3 ? styles.activeColor : ''}`}/>
+</ul>
+<div className={styles.deliveryInfo}>Бесплатная доставка - <span>{getCurrentDate('.')}</span></div>    
+<div className={styles.controlOrderBox}>
+  <button 
+    className="greenButton"
+    onClick={() => { 
+    isItemAdded(item.productId) ? 
+    setCartOpened(true) : 
+    onAddToCard(item)}}>
+  { isItemAdded(item.productId) ?  'Товар в корзине' : 'Добавить в корзину'}
+  {
+    isItemAdded(item.productId) && 
+    <img 
+    className="arrowOrder" 
+    width="14" 
+    height="12" 
+    src="img/arrow.svg" 
+    alt="Arrow"
+    />
     }
-
-    const changeColor = (num) => {
-
-        setCheckedColor1(false)
-        setCheckedColor2(false)
-        setCheckedColor3(false)
-
-        switch (num) {
-                case 1: setCheckedColor1(true) 
-                break;
-                case 2: setCheckedColor2(true)
-                break;
-                case 3: setCheckedColor3(true)   
-                break;
-            default:
-                break;
-        }
-    }
-
-    return (
-        <div className="content">
-            {isLoading ? <GlobalLoader /> : (
-                    <>
-                    <div className={styles.detailProductContent}>
-                        <Link to={`${showCase}`}>
-                            <img 
-                                //onClick={() => navigate(-1)}
-                                className={styles.backBtn} 
-                                width="35" 
-                                height="35" 
-                                src="img/btn-back.svg" 
-                                alt="Back"
-                            />
-                        </Link>
-                        <h1 className={styles.titleContent}>{item.title}</h1>
-                    </div> 
-
-                    <div className={styles.detailPageWrapper}>
-                        <div className={styles.imgProductBox}>
-                            <img 
-                                width={133} 
-                                src={item.imageUrl}
-                            />
-                        </div>
-                        <div className={styles.infoProductBox}>
-                            <div className={styles.productPrice}>{item.price} ₽</div>
-                            <div className={styles.colorTitle}>Цвет:</div>
-                            <div className={styles.colorBox}>
-                                <div onClick={() => changeColor(1)} className={`${styles.colorBoxItem} ${checkedColor1 ? styles.activeColor : ''}`}></div>
-                                <div onClick={() => changeColor(2)} className={`${styles.colorBoxItem} ${checkedColor2 ? styles.activeColor : ''}`}></div>
-                                <div onClick={() => changeColor(3)} className={`${styles.colorBoxItem} ${checkedColor3 ? styles.activeColor : ''}`}></div>
-                            </div>
-                            <div className={styles.deliveryInfo}>Бесплатная доставка - <span>{getCurrentDate('.')}</span></div>    
-
-                            <div className={styles.controlOrderBox}>
-                                <button 
-                                    className="greenButton"
-                                    onClick={() => { 
-                                        isItemAdded(item.productId) ? 
-                                        setCartOpened(true) : 
-                                        onAddToCard(item)} 
-                                    }
-                                    //disabled={isItemAdded(item.productId)}
-                                >
-                                    { isItemAdded(item.productId) ?  'Товар в корзине' : 'Добавить в корзину'}
-                                {
-                                  isItemAdded(item.productId) && 
-                                        <img 
-                                        className="arrowOrder" 
-                                        width="14" 
-                                        height="12" 
-                                        src="img/arrow.svg" 
-                                        alt="Arrow"
-                                    />
-                                    
-                                }
-                                </button>
-                                <div className={styles.addBox}> 
-                                {isLoadingFavorite && <GlobalLoader smalMode={true}/>}
-                                    <img 
-                                        width="55" 
-                                        height="55" 
-                                        onClick={onClickFovarite}
-                                        src={ 
-                                            isItemFavorited(idInUrl) ? 
-                                            "img/liked.svg" : 
-                                            "img/unliked.svg" 
-                                        }
-                                        alt="Unliked"
-                                    />
-                                </div>
-                            </div>  
-                        </div>
-
-                    </div>   
-                    </>
-                )}
-
-        </div>
-        )
-    }
-    
-export default Detail
+  </button>
+<div className={styles.addBox}> 
+  {isLoadingFavorite && <GlobalLoader smalMode={true}/>}
+  <img 
+  width="55" 
+  height="55" 
+  onClick={onClickFovarite}
+  src={ 
+  isItemFavorited(idInUrl) ? 
+  "img/liked.svg" : 
+  "img/unliked.svg" 
+  }
+  alt="Unliked"
+  />
+</div>
+</div>  
+</div>
+</div>   
+</>
+)}
+</div>
+)
+}
