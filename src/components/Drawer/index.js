cb220d67590ac57df3312b0cd42b925fbc0972a0
Яков Носков <jakovnoskov@hotmail.com>
2022-10-13 23:03:25 +0600

    useOnClickOutside +blur overlay

--- src/components/Drawer/index.js
+++ src/components/Drawer/index.js
@@ -1,11 +1,12 @@
-import React from 'react'
+import { useRef, useState, useContext, useEffect } from 'react'
 import axios from 'axios'
 
 import GlobalLoader from '../GlobalLoader'
 import AppContext from '../../context'
 import Info from '../Info'
-import {useCart} from '../../hooks/useCart'
-import {getCurrentDate} from '../../utils/getCurrentDate'
+import { useCart } from '../../hooks/useCart'
+import useOnClickOutside from '../../hooks/useOnClickOutside'
+import { getCurrentDate } from '../../utils/getCurrentDate'
 
 import styles from './Drawer.module.scss'
 
@@ -15,37 +16,39 @@ const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
 
 export default function Drawer() {
   const { cartItems, setCartItems, totalPrice } = useCart()
-  const [ orderId, setOrderId] = React.useState(null)
-  const [ isOrderComplite, setIsOrderComplite] = React.useState(false)
-  const [ isLoading, setisLoading] = React.useState(false)
-  const { 
-    setGlobalLoading, 
-    cartLoading, 
-    cartOpened, 
-    setCartOpened, 
-    onRemoveItem 
-  } = React.useContext(AppContext)
+  const [orderId, setOrderId] = useState(null)
+  const [isOrderComplite, setIsOrderComplite] = useState(false)
+  const [isLoading, setisLoading] = useState(false)
+  const dawerRef = useRef(null)
+  useOnClickOutside(dawerRef, () => setCartOpened(false))
+  const {
+    setGlobalLoading,
+    cartLoading,
+    cartOpened,
+    setCartOpened,
+    onRemoveItem
+  } = useContext(AppContext)
 
   const onClickOrder = async () => {
     try {
-        setisLoading(true)
-        setGlobalLoading(true)
-        const { data } = await axios.post(ordersUrl, {
-            totalPrice: totalPrice,
-            date: getCurrentDate('.'),
-            items: cartItems 
-        })
-        
-        setOrderId(data.id)
+      setisLoading(true)
+      setGlobalLoading(true)
+      const { data } = await axios.post(ordersUrl, {
+        totalPrice: totalPrice,
+        date: getCurrentDate('.'),
+        items: cartItems
+      })
 
-        for (let i = 0; i < cartItems.length; i++) {
-            const item = cartItems[i]
-            await axios.delete(`${cartUrl}/${item.id}`)
-            await delay(200)
-        }
+      setOrderId(data.id)
+
+      for (let i = 0; i < cartItems.length; i++) {
+        const item = cartItems[i]
+        await axios.delete(`${cartUrl}/${item.id}`)
+        await delay(200)
+      }
     } catch (error) {
-        console.log(error)
-        alert('Ошибка при создании заказа :(')
+      console.log(error)
+      alert('Ошибка при создании заказа :(')
     }
     setCartItems([])
     setIsOrderComplite(true)
@@ -55,102 +58,110 @@ export default function Drawer() {
     setIsOrderComplite(false)
   }
 
-    React.useEffect(() => {
-    if(!cartOpened) setIsOrderComplite(false)
-    }, [cartOpened])
+  useEffect(() => {
+    if (!cartOpened) setIsOrderComplite(false)
+  }, [cartOpened])
 
-    return (
-    <div className={`${styles.overlay} ${cartOpened ? styles.overlayVisible : ''}`}>
-        <div className={styles.drawer}>
+  // useEffect(() => {
+  //   useOnClickOutside(dawerRef, () => setCartOpened(false))
+  // }, [cartOpened])
+
+  return (
+    <div
+      className={`${styles.overlay} ${cartOpened ? styles.overlayVisible : ''}`}>
+      <div
+        ref={dawerRef}
+        className={styles.drawer}
+      >
         <h2 className={styles.titleCart}>
-        Корзина
-            <img 
-                onClick={() => setCartOpened(false)}
-                className={styles.removeBtn} 
-                width="32" 
-                height="32" 
-                src="img/btn-remove.svg" 
-                alt="Close"
-            />
+          Корзина
+          <img
+            onClick={() => setCartOpened(false)}
+            className={styles.removeBtn}
+            width="32"
+            height="32"
+            src="img/btn-remove.svg"
+            alt="Close"
+          />
         </h2>
 
-        {cartLoading && <GlobalLoader /> }
-        
-        {cartItems.length > 0 ? (
-            <>
-            <div className={styles.items}>
-                {cartItems.map((obj, index) => (
-                    <div key={index} className={styles.cartItem}>
-                        <div 
-                            style={{backgroundImage:`url(${obj.imageUrl})`}} 
-                            className={styles.cartImg} >
-                        </div>
+        {cartLoading && <GlobalLoader />}
 
-                        <div className={styles.cartItemText}>
-                            <p>{obj.title}</p>
-{/*
+        {cartItems.length > 0 ? (
+          <>
+            <div className={styles.items}>
+              {cartItems.map((obj, index) => (
+                <div key={index} className={styles.cartItem}>
+                  <div
+                    style={{ backgroundImage: `url(${obj.imageUrl})` }}
+                    className={styles.cartImg} >
+                  </div>
+
+                  <div className={styles.cartItemText}>
+                    <p>{obj.title}</p>
+                    {/*
                             <p>id: {obj.id}</p>
                             <p>productId: {obj.productId}</p>
 */}
-                            <b>{obj.price} руб.</b>
-                        </div>
+                    <b>{obj.price} руб.</b>
+                  </div>
 
-                        <img 
-                            onClick={() => onRemoveItem(obj)}
-                            className={styles.removeBtn} 
-                            width="32" 
-                            height="32" 
-                            src="img/btn-remove.svg" 
-                            alt="Remove"
-                        />
-                    </div>
-                ))}
-                
-            </div>  
+                  <img
+                    onClick={() => onRemoveItem(obj)}
+                    className={styles.removeBtn}
+                    width="32"
+                    height="32"
+                    src="img/btn-remove.svg"
+                    alt="Remove"
+                  />
+                </div>
+              ))}
+
+            </div>
 
             <div className={styles.cartTotalBlock}>
-            <ul className={styles.cartTotalPriceList}>
-            <li className={styles.cartTotalPriceListItem}>
-                <span>Итого:</span>
-                <div className={styles.bbsLine}></div>
-                <b>{totalPrice} руб. </b>
-            </li>
-            <li className={styles.cartTotalPriceListItem}>
-                <span>Налог 5%: </span>
-                <div className={styles.bbsLine}></div>
-                <b>{Math.round((totalPrice / 100) * 5)} руб. </b>
-            </li>
-            </ul>
-            <button 
+              <ul className={styles.cartTotalPriceList}>
+                <li className={styles.cartTotalPriceListItem}>
+                  <span>Итого:</span>
+                  <div className={styles.bbsLine}></div>
+                  <b>{totalPrice} руб. </b>
+                </li>
+                <li className={styles.cartTotalPriceListItem}>
+                  <span>Налог 5%: </span>
+                  <div className={styles.bbsLine}></div>
+                  <b>{Math.round((totalPrice / 100) * 5)} руб. </b>
+                </li>
+              </ul>
+              <button
                 disabled={isLoading}
                 onClick={onClickOrder}
                 className={styles.greenButton}>
                 Оформить заказ
-                <img 
-                    className={styles.arrowOrder} 
-                    width="14" 
-                    height="12" 
-                    src="img/arrow.svg" 
-                    alt="Arrow"
+                <img
+                  className={styles.arrowOrder}
+                  width="14"
+                  height="12"
+                  src="img/arrow.svg"
+                  alt="Arrow"
                 />
-            </button>
+              </button>
             </div>
-            </>            
+          </>
         ) : (
-            isOrderComplite ? 
-            <Info 
-                image={"img/complete-order.jpg"}
-                title={"Заказ оформлен!"}
-                description={`Ваш заказ #${orderId} скоро будет передан курьерской доставке`}
+          isOrderComplite ?
+            <Info
+              image={"img/complete-order.jpg"}
+              title={"Заказ оформлен!"}
+              description={`Ваш заказ #${orderId} скоро будет передан курьерской доставке`}
             />
             :
-            <Info 
-                image={"img/empty-cart.jpg"}
-                title={"Корзина пустая"}
-                description={"Добавьте хотя бы одну пару кросовок что-бы сделать заказ"}
+            <Info
+              image={"img/empty-cart.jpg"}
+              title={"Корзина пустая"}
+              description={"Добавьте хотя бы одну пару кросовок что-бы сделать заказ"}
             />
-    )}
-        </div>
+        )}
+      </div>
     </div>
-    )
+  )
 }
